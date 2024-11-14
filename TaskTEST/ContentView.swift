47a58030df30fef559261ceaea06f3b842b69a10
Kim JaeHyung <you3192001@icloud.com>
2024-11-14 16:35:59 +0900

    Initial Commit

---
+++ TaskTEST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTEST
+//
+//  Created by Jae hyung Kim on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTEST/TaskTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTESTApp.swift
+//  TaskTEST
+//
+//  Created by Jae hyung Kim on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
